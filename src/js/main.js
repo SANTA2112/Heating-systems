1d9618567ec5d67866c172548ba046565d5945cf
Santa <s.yushmanov777@gmail.com>
2018-07-14 20:24:10 +0700

    somefix

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,6 @@ qa('.tab_btn').forEach((tab, i) => tab.onclick = () => {
   dis(qa('.tab_btn')); dis(qa('.servises__type-of-servises')); add(tab); add(qa('.servises__type-of-servises')[i])
 })
 
-
 // slider-images
 
 const sliderImages = document.querySelectorAll('.sliderContent__small-img');
@@ -21,9 +20,6 @@ sliderContent.forEach(el => el.onmouseover = even => {
     sliderMainImage.setAttribute(d, img.getAttribute(d));
   })
 })
-
-
-
 
 //certificates popap
 
@@ -42,7 +38,7 @@ const closeMod = modals => {
     }
   })
 }
-
+closeMod(qa('.modal'))
 qa('.certificate-block__elem').forEach(el => el.onclick = e => {
   modalCert.style.display = 'flex';
   modalImg.setAttribute('src', e.target.getAttribute('data-src'))
@@ -50,7 +46,7 @@ qa('.certificate-block__elem').forEach(el => el.onclick = e => {
   if(document.body.offsetWidth > 768) document.body.style.paddingRight = `${padding}px`;
   document.body.style.overflow = 'hidden';
 })
-closeMod(qa('.modal'))
+
 
 const modalWork = document.querySelector('.modal.modal-works');
 const modalCallback = document.querySelector('.modal.modal-callback');
@@ -73,51 +69,30 @@ showForm(modalWork, ButtonsWork)
 showForm(modalCallback, ButtonsCallback)
 showForm(modalSpecialist, ButtonsSpecialist)
 showForm(modalCons, ButtonsCons)
-//forms
-/*
-const formModal1 = document.querySelector('.modal .form-works');
+
+// select
+const select = qa('.form-works__select');
+const selectLabel = qa('.form-works__select-label');
+const selectOptions = qa('.form-works__select-values');
+select.forEach(select => select.onclick = e => select.classList.toggle('active'));
+select.forEach(select => select.onblur = e => select.classList.remove('active'));
+
+selectOptions.forEach((option, i) => option.onclick = e => {
+  if (e.target === e.currentTarget) return null;
+  const input = e.target.closest('.form-works__select').querySelector('input');
+  selectLabel[i].textContent = e.target.textContent;
+  input.value = e.target.textContent;
+  select[i].dispatchEvent(new CustomEvent('change', {detail: { value: e.target.textContent }}))
+})
+
 const formBtns = document.querySelector('.servises__tabs-content');
 const formBtns1 = document.querySelector('.mobile-slider');
-const formWorks = document.querySelector('.modal .form-works');
-const formCallBack = document.querySelector('.form-callback');
-const formCons = document.querySelector('.form-cons');
-const formSpecialist = document.querySelector('.form-specialist');
-const select = document.querySelectorAll('.form-works__select');
-const selectLabel = document.querySelectorAll('.form-works__select-label');
-const selectOptions1 = document.querySelectorAll('.form-works__select-values');
 const tabMenu = [...document.querySelectorAll('.servises__type-of-servises')];
 const mobileTab = [...document.querySelectorAll('.mobile-slider .swiper-wrapper[data-tab]')];
-
-select[0].addEventListener('click', e => {
-  select[0].classList.toggle('active');
-});
-select[1].addEventListener('click', e => {
-  select[1].classList.toggle('active');
-});
-
-selectOptions1[0].addEventListener('click', e => {
-  if (e.target === e.currentTarget) return null;
-  const input = e.target.closest('.form-works__select').querySelector('input');
-  selectLabel[0].textContent = e.target.textContent;
-  input.value = e.target.textContent;
-  select[0].dispatchEvent(new CustomEvent('change', {detail: { value: e.target.textContent }}))
-})
-selectOptions1[1].addEventListener('click', e => {
-  if (e.target === e.currentTarget) return null;
-  const input = e.target.closest('.form-works__select').querySelector('input');
-  selectLabel[1].textContent = e.target.textContent;
-  input.value = e.target.textContent;
-  select[1].dispatchEvent(new CustomEvent('change', {detail: { value: e.target.textContent }}))
-})
-
-select[0].addEventListener('blur', () => select[0].classList.remove('active'))
-select[1].addEventListener('blur', () => select[1].classList.remove('active'))
 
 
 formBtns.addEventListener('click', e => {
   if(e.target.classList.contains('servise__link')) {
-    formModal1.style.display = 'flex';
-    formWorks.style.marginLeft = 0;
     const activeTabMenu = tabMenu.filter(el => el.classList.contains('active'));
     if(activeTabMenu[0].getAttribute('data-tab') == 1) {
       selectLabel[1].textContent = 'Монтаж';
@@ -135,15 +110,10 @@ formBtns.addEventListener('click', e => {
       selectLabel[1].textContent = 'Обслуживание'
       selectLabel[1].nextElementSibling.value ='Обслуживание';
     }
-    setTimeout(() => formModal1.classList.add('show'), 0);
-    document.body.style.paddingRight = `${padding}px`;
-    document.body.style.overflow = 'hidden';
   }
 })
 formBtns1.addEventListener('click', e => {
   if(e.target.classList.contains('servise__link')) {
-    formModal1.style.display = 'flex';
-    formWorks.style.marginLeft = 0;
     const mobileActiveTab = mobileTab.filter(el => el.parentElement.parentElement.classList.contains('swiper-slide-active'))
     if(mobileActiveTab[0].getAttribute('data-tab') == 1) {
       selectLabel[1].textContent = 'Монтаж'
@@ -161,22 +131,20 @@ formBtns1.addEventListener('click', e => {
       selectLabel[1].textContent = 'Обслуживание'
       selectLabel[1].nextElementSibling.value ='Обслуживание';
     }
-    setTimeout(() => formModal1.classList.add('show'), 0);
-    document.body.style.paddingRight = `${padding}px`;
-    document.body.style.overflow = 'hidden';
   }
-})*/
+})
 
 
 
 //slider
 
-const swiper = new Swiper('.swiper-container', {
+const swiperWorks = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.button-next',
     prevEl: '.button-prev',
   },
 });
+
 
 [...document.querySelectorAll('form')].forEach(form => form.onsubmit = e => {
   e.preventDefault();
